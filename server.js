a3e59cd29cdf9e266737cc08766d4186be0acdf3
Troy <troy.watt@ultradent.com>
2015-02-25 07:08:18 +0000

    adds Jobs model and mongo db connection configuration

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require( 'express' );
+var mongoose = require("mongoose");
+var job = require( './models/Jobs' );
 
 var app = express();
 
@@ -7,11 +9,29 @@ app.set( 'view engine', 'jade' );
 
 app.use( express.static( __dirname + '/public' ) );
 
+
+app.get( '/api/jobs', function( req, res ){
+    
+    mongoose.model( 'Job' ).find({}).exec(function( error, collection  ){
+        res.send( collection );
+    });   
+    
+})
+
+// Catch all
 // angular is handling our routes for this example app to keep it simple
 app.get( '*', function( req, res ){
     res.render( 'index' );
 } );
 
+// mongoose.connect('mongodb://localhost/jobfinder');
+mongoose.connect('mongodb://root:password@ds049161.mongolab.com:49161/jobfinder');
 
+var conn = mongoose.connection;
+
+conn.once( 'open', function(){
+    console.log('mongoose connected successfully');
+    job.seedJobs();
+})
 
 app.listen( process.env.PORT, process.env.IP );
